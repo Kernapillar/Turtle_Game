a93898a4450561974234c3f30280059b06a87127
Alexander Kern <alexander.t.kern@gmail.com>
2026-06-13 18:36:02 -0500

    Movement and blurring fix

--- Scenes/turtle.gd
+++ Scenes/turtle.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
-var direction:= Vector2(3, 4)
-var speed = 40 
+var max_speed := 500.0
+var acceleration:= 0.4
+var breaking := 0.2
+var direction := Vector2(0,1)
 
 
 func _physics_process(delta):
-	velocity = direction * speed
+	handle_movement_input()
 	move_and_slide()
+	
+func handle_movement_input(): 
+	direction = Input.get_vector("Left", "Right", "Up", "Down") 
+	if direction.length() > 0: 
+		$AnimationPlayer.play("Walk")
+		velocity = velocity.lerp(direction * max_speed, acceleration)
+	else: 
+		velocity = velocity.lerp(Vector2.ZERO, breaking)	
+		$AnimationPlayer.play("Idle")
